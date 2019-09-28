46939f62f09f10c98d48c2abb631cb3dc31c3b33
ywqy <aaawuwu@126.com>
2019-09-28 21:18:16 +0800

    Tile Content

---
+++ 01 The Board/03 ChangingTiles/Assets/Scripts/GameBoard.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoard : MonoBehaviour {
+    [SerializeField]
+    Transform ground = default;
+    [SerializeField]
+    GameTile tilePrefab = default;
+
+    Vector2Int size;
+    GameTile[] tiles;
+    Queue<GameTile> searchFrontier = new Queue<GameTile>();
+
+
+    public void Initialize(Vector2Int size) {
+        this.size = size;
+        ground.localScale = new Vector3(size.x, size.y, 1f);
+
+        Vector2 offset = new Vector2(
+            (size.x - 1) * 0.5f, (size.y - 1) * 0.5f
+        );
+        tiles = new GameTile[size.x * size.y];
+        for (int i = 0, y = 0; y < size.y; y++) {
+            for (int x = 0; x < size.x; x++, i++) {
+                GameTile tile = tiles[i] = Instantiate(tilePrefab);
+                tile.transform.SetParent(transform, false);
+                tile.transform.localPosition = new Vector3(
+                    x - offset.x, 0, y - offset.y
+                );
+
+                if (x > 0) {
+                    GameTile.MakeEastWestNeighbors(tile, tiles[i - 1]);
+                }
+                if (y > 0) {
+                    GameTile.MakeNorthSouthNeighbors(tile, tiles[i - size.x]);
+                }
+                tile.IsAlternative = (x & 1) == 0;
+                if ((y & 1) == 0) {
+                    tile.IsAlternative = !tile.IsAlternative;
+                }
+            }
+        }
+        FindPaths();
+    }
+
+    void FindPaths() {
+        foreach (GameTile tile in tiles) {
+            tile.ClearPath();
+        }
+        tiles[tiles.Length / 2].BecomeDestination();
+        searchFrontier.Enqueue(tiles[tiles.Length / 2]);
+
+        while (searchFrontier.Count > 0) {
+            GameTile tile = searchFrontier.Dequeue();
+            if (tile != null) {
+                if (tile.IsAlternative) {
+                    searchFrontier.Enqueue(tile.GrowPathNorth());
+                    searchFrontier.Enqueue(tile.GrowPathSouth());
+                    searchFrontier.Enqueue(tile.GrowPathEast());
+                    searchFrontier.Enqueue(tile.GrowPathWest());
+                } else {
+                    searchFrontier.Enqueue(tile.GrowPathWest());
+                    searchFrontier.Enqueue(tile.GrowPathEast());
+                    searchFrontier.Enqueue(tile.GrowPathSouth());
+                    searchFrontier.Enqueue(tile.GrowPathNorth());
+                }
+            }
+        }
+
+        foreach (GameTile tile in tiles) {
+            tile.ShowPath();
+        }
+    }
+}
---
+++ 01 The Board/03 ChangingTiles/Assets/Scripts/GameTile.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTile : MonoBehaviour {
+    static Quaternion
+        northRotation = Quaternion.Euler(90f, 0f, 0f),
+        eastRotation = Quaternion.Euler(90f, 90f, 0f),
+        southRotation = Quaternion.Euler(90f, 180f, 0f),
+        westRotation = Quaternion.Euler(90f, 270f, 0f);
+
+    [SerializeField]
+    Transform arrow = default;
+
+    GameTile north, east, south, west, nextOnPath;
+    int distance;
+
+    public bool HasPath => distance != int.MaxValue;
+    public bool IsAlternative { get; set; }
+    public GameTile GrowPathNorth() => GrowPathTo(north);
+    public GameTile GrowPathEast() => GrowPathTo(east);
+    public GameTile GrowPathSouth() => GrowPathTo(south);
+    public GameTile GrowPathWest() => GrowPathTo(west);
+
+    public static void MakeEastWestNeighbors(GameTile east, GameTile west) {
+        Debug.Assert(west.east == null && east.west == null, "Redefined Neighbors!");
+        west.east = east;
+        east.west = west;
+    }
+
+    public static void MakeNorthSouthNeighbors(GameTile north, GameTile south) {
+        Debug.Assert(north.south == null && south.north == null, "Redefined Neighbors!");
+        south.north = north;
+        north.south = south;
+
+    }
+
+    public void ClearPath() {
+        distance = int.MaxValue;
+        nextOnPath = null;
+    }
+
+    public void BecomeDestination() {
+        distance = 0;
+        nextOnPath = null;
+    }
+
+    public void ShowPath() {
+        if (distance == 0) {
+            arrow.gameObject.SetActive(false);
+            return;
+        }
+        arrow.gameObject.SetActive(true);
+        arrow.localRotation =
+            nextOnPath == north ? northRotation :
+            nextOnPath == east ? eastRotation :
+            nextOnPath == south ? southRotation :
+            westRotation;
+    }
+
+    GameTile GrowPathTo(GameTile neighbor) {
+        Debug.Assert(HasPath, "No path!");
+        if (!HasPath || neighbor == null || neighbor.HasPath) { return null; }
+        neighbor.distance = distance + 1;
+        neighbor.nextOnPath = this;
+        return neighbor;
+    }
+
+
+
+
+}

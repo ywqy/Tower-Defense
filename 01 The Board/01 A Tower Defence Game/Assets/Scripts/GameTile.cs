303f499784da12ad6f718b12bae66cde6cc9db9d
ywqy <aaawuwu@126.com>
2019-09-28 11:08:06 +0800

    Tile Prefab

---
+++ 01 The Board/01 A Tower Defence Game/Assets/Scripts/GameTile.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTile : MonoBehaviour
+{
+    [SerializeField]
+    Transform arrow = default;
+}

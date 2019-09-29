e0140088e1ad62b5ecb264e9d2d168d171f03a0f
ywqy <aaawuwu@126.com>
2019-09-29 09:05:15 +0800

    Touching a Tile

--- 01 The Board/03 ChangingTiles/Assets/Scripts/GameBoard.cs
+++ 01 The Board/03 ChangingTiles/Assets/Scripts/GameBoard.cs
@@ -44,6 +44,18 @@ public class GameBoard : MonoBehaviour {
         FindPaths();
     }
 
+    public GameTile GetTile(Ray ray) {
+        if (Physics.Raycast(ray,out RaycastHit hit)) {
+            int x = (int)(hit.point.x + size.x * .5f);
+            int y = (int)(hit.point.z + size.y * .5f);
+            if (x >= 0 && x < size.x && y >= 0 && y < size.y) {
+                return tiles[x + y * size.x];
+            }
+            return null;
+        }
+        return null;
+    }
+
     void FindPaths() {
         foreach (GameTile tile in tiles) {
             tile.ClearPath();

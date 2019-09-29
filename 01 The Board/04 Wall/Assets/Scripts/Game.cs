8a72edbba4595f109153705fe3ff454ae6b2eae6
ywqy <aaawuwu@126.com>
2019-09-29 10:27:45 +0800

    04 Wall

---
+++ 01 The Board/04 Wall/Assets/Scripts/Game.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+    [SerializeField]
+    Vector2Int boardSize = new Vector2Int(11, 11);
+    [SerializeField]
+    GameBoard board = default;
+    [SerializeField]
+    GameTileContentFactory tileContentFactory = default;
+
+    Ray TouchRay => Camera.main.ScreenPointToRay(Input.mousePosition);
+
+    void Awake() {
+        board.Initialize(boardSize,tileContentFactory);
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            HandleTouch();
+        }
+    }
+
+    void HandleTouch() {
+        GameTile tile = board.GetTile(TouchRay);
+        if (tile != null) {
+            board.ToggleDestination(tile);
+        }
+    }
+
+    void OnValidate() {
+        if (boardSize.x < 2) { boardSize.x = 2; }
+        if (boardSize.y < 2) { boardSize.y = 2; }
+    }
+}

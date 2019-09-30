ab9bb5aef2b2a0c6efead4c73f4a3dcbfb3aee07
ywqy <aaawuwu@126.com>
2019-09-30 18:03:49 +0800

    Factories

---
+++ 02 Enemies/02 Spawn Enemies/Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    EnemyFactory originFactory;
+
+    public EnemyFactory OriginFactory {
+        get => originFactory;
+        set {
+            Debug.Assert(originFactory == null, "Redefined origin factory");
+            originFactory = value;
+        }
+    }
+}
---
+++ 02 Enemies/02 Spawn Enemies/Assets/Scripts/EnemyFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFactory : GameObjectFactory
+{
+    [SerializeField]
+    Enemy prefab = default;
+
+    public Enemy Get() {
+        Enemy instance = CreateGameObjectInstance(prefab);
+        instance.OriginFactory = this;
+        return instance;
+    }
+
+    public void Reclaim(Enemy enemy) {
+        Debug.Assert(enemy.OriginFactory == this, "Wrong factory reclaimed!");
+        Destroy(enemy.gameObject);
+    }
+}

aa9c993e1ac729fc5c88d2fc59f229a0f9e82d11
ywqy <aaawuwu@126.com>
2019-09-30 20:46:11 +0800

    Moving Enemies

---
+++ 02 Enemies/03 Moving Enemies/Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    EnemyFactory originFactory;
+
+    public EnemyFactory OriginFactory {
+        get => originFactory;
+        set {
+            Debug.Assert(originFactory == null, "Redefined origin factory");
+            originFactory = value;
+        }
+    }
+
+    public void SpawnOn(GameTile tile) {
+        transform.localPosition = tile.transform.localPosition;
+    }
+}

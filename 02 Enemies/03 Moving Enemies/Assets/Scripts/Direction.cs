d7164560612e3607bbd6486c6dc947df9fa4715c
ywqy <aaawuwu@126.com>
2019-10-01 16:15:06 +0800

    Changing Direction

--- 02 Enemies/03 Moving Enemies/Assets/Scripts/Direction.cs
+++ 02 Enemies/03 Moving Enemies/Assets/Scripts/Direction.cs
@@ -6,6 +6,10 @@ public enum Direction {
     North, East, South, West
 }
 
+public enum DirectionChange {
+    None, TurnRight, TurnLeft, TurnAround
+}
+
 public static class DirectionExtensions {
     static Quaternion[] rotations = {
         Quaternion.identity,
@@ -16,5 +20,20 @@ public static class DirectionExtensions {
 
     public static Quaternion GetRotation(this Direction direction) {
         return rotations[(int)direction];
+    }
+
+    public static DirectionChange GetDirectionChangeTo(this Direction current, Direction next) {
+        if(current == next) {
+            return DirectionChange.None;
+        }else if(current+1==next|| current - 3 == next) {
+            return DirectionChange.TurnRight;
+        }else if(current-1==next|| current + 3 == next) {
+            return DirectionChange.TurnLeft;
+        }
+        return DirectionChange.TurnAround;
+    }
+
+    public static float GetAngle(this Direction direction) {
+        return (float)direction * 90f;
     }
 }
--- 02 Enemies/03 Moving Enemies/Assets/Scripts/Enemy.cs
+++ 02 Enemies/03 Moving Enemies/Assets/Scripts/Enemy.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
-{
+public class Enemy : MonoBehaviour {
     EnemyFactory originFactory;
     GameTile tileFrom, tileTo;
     Vector3 positionFrom, positionTo;
     float progress;
+    Direction direction;
+    DirectionChange directionChange;
+    float directionAngleFrom, directionAngleTo;
 
     public EnemyFactory OriginFactory {
         get => originFactory;
@@ -21,10 +23,9 @@ public class Enemy : MonoBehaviour
         Debug.Assert(tile.NextTileOnPath != null, "Nowhere to go!", this);
         tileFrom = tile;
         tileTo = tile.NextTileOnPath;
-        positionFrom = tileFrom.transform.localPosition;
-        positionTo = tileFrom.ExitPoint;
-        transform.localRotation = tileFrom.PathDirection.GetRotation();
         progress = 0f;
+        PrepareIntro();
+
     }
 
     public bool GameUpdate() {
@@ -32,16 +33,60 @@ public class Enemy : MonoBehaviour
         while (progress >= 1f) {
             tileFrom = tileTo;
             tileTo = tileTo.NextTileOnPath;
-            if(tileTo == null) {
+            if (tileTo == null) {
                 OriginFactory.Reclaim(this);
                 return false;
             }
-            positionFrom = positionTo;
-            positionTo = tileFrom.ExitPoint;
-            transform.localRotation = tileFrom.PathDirection.GetRotation();
+
             progress -= 1f;
+            PrepareNextState();
         }
         transform.localPosition = Vector3.LerpUnclamped(positionFrom, positionTo, progress);
+        if (directionChange != DirectionChange.None) {
+            float angle = Mathf.LerpUnclamped(directionAngleFrom, directionAngleTo, progress);
+            transform.localRotation = Quaternion.Euler(0f, angle, 0f);
+        }
         return true;
+    }
+
+    void PrepareIntro() {
+        positionFrom = tileFrom.transform.localPosition;
+        positionTo = tileFrom.ExitPoint;
+        direction = tileFrom.PathDirection;
+        directionChange = DirectionChange.None;
+        directionAngleFrom = directionAngleTo = direction.GetAngle();
+        transform.localRotation = direction.GetRotation();
+    }
+
+    void PrepareNextState() {
+        positionFrom = positionTo;
+        positionTo = tileFrom.ExitPoint;
+        directionChange = direction.GetDirectionChangeTo(tileFrom.PathDirection);
+        direction = tileFrom.PathDirection;
+        directionAngleFrom = directionAngleTo;
+
+        switch (directionChange) {
+            case DirectionChange.None: PrepareForward(); break;
+            case DirectionChange.TurnRight: PrepareTurnRight(); break;
+            case DirectionChange.TurnLeft: PrepareTurnLeft(); break;
+            default: PrepareTurnAround(); break;
+        }
+    }
+
+    void PrepareForward() {
+        transform.localRotation = direction.GetRotation();
+        directionAngleTo = direction.GetAngle();
+    }
+
+    void PrepareTurnRight() {
+        directionAngleTo = directionAngleFrom + 90f;
+    }
+
+    void PrepareTurnLeft() {
+        directionAngleTo = directionAngleFrom - 90f;
+    }
+
+    void PrepareTurnAround() {
+        directionAngleTo = directionAngleFrom + 180f;
     }
 }

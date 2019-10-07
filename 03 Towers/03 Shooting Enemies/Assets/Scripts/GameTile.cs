a0827bc540f9affbd9dfddb389aedf6699bcbb36
ywqy <aaawuwu@126.com>
2019-10-07 21:12:47 +0800

    Shooting Enemies

---
+++ 03 Towers/03 Shooting Enemies/Assets/Scripts/GameTile.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTile : MonoBehaviour {
+    static Quaternion
+        northRotation = Quaternion.Euler(90f, 0f, 0f),
+        eastRotation = Quaternion.Euler(90f, 90f, 0f),
+        southRotation = Quaternion.Euler(90f, 180f, 0f),
+        westRotation = Quaternion.Euler(90f, 270f, 0f);
+
+    [SerializeField]
+    Transform arrow = default;
+
+    GameTile north, east, south, west, nextOnPath;
+    int distance;
+    GameTileContent content;
+
+    public bool HasPath => distance != int.MaxValue;
+    public GameTile NextTileOnPath => nextOnPath;
+    public bool IsAlternative { get; set; }
+    public GameTile GrowPathNorth() => GrowPathTo(north, Direction.South);
+    public GameTile GrowPathEast() => GrowPathTo(east, Direction.West);
+    public GameTile GrowPathSouth() => GrowPathTo(south, Direction.North);
+    public GameTile GrowPathWest() => GrowPathTo(west, Direction.East);
+    public GameTileContent Content {
+        get => content;
+        set {
+            Debug.Assert(value != null, "Null assigned to content");
+            if (content != null) {
+                content.Recycle();
+            }
+            content = value;
+            content.transform.localPosition = transform.localPosition;
+        }
+    }
+    public Vector3 ExitPoint { get; private set; }
+    public Direction PathDirection { get; private set; }
+
+    public static void MakeEastWestNeighbors(GameTile east, GameTile west) {
+        Debug.Assert(west.east == null && east.west == null, "Redefined Neighbors!");
+        west.east = east;
+        east.west = west;
+    }
+
+    public static void MakeNorthSouthNeighbors(GameTile north, GameTile south) {
+        Debug.Assert(north.south == null && south.north == null, "Redefined Neighbors!");
+        south.north = north;
+        north.south = south;
+
+    }
+
+    public void ClearPath() {
+        distance = int.MaxValue;
+        nextOnPath = null;
+    }
+
+    public void BecomeDestination() {
+        distance = 0;
+        nextOnPath = null;
+        ExitPoint = transform.localPosition;
+    }
+
+    public void ShowPath() {
+        if (distance == 0) {
+            arrow.gameObject.SetActive(false);
+            return;
+        }
+        arrow.gameObject.SetActive(true);
+        arrow.localRotation =
+            nextOnPath == north ? northRotation :
+            nextOnPath == east ? eastRotation :
+            nextOnPath == south ? southRotation :
+            westRotation;
+    }
+
+    public void HidePath() {
+        arrow.gameObject.SetActive(false);
+    }
+
+    GameTile GrowPathTo(GameTile neighbor, Direction direction) {
+        Debug.Assert(HasPath, "No path!");
+        if (!HasPath || neighbor == null || neighbor.HasPath) { return null; }
+        neighbor.distance = distance + 1;
+        neighbor.nextOnPath = this;
+        neighbor.ExitPoint = neighbor.transform.localPosition + direction.GetHalfVector();
+        neighbor.PathDirection = direction;
+        return neighbor.content.BlocksPath ? null : neighbor;
+    }
+
+
+
+
+}
---
+++ 03 Towers/03 Shooting Enemies/Assets/Scripts/Tower.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Tower : GameTileContent {
+    const int enemyLayerMask = 1 << 9;
+    static Collider[] targetsBuffer = new Collider[1];
+
+    [SerializeField, Range(1.5f, 10.5f)]
+    float targetingRange = 1.5f;
+
+    TargetPoint target;
+
+
+    public override void GameUpdate() {
+        if (TrackTarget() || AcquireTarget()) {
+            Debug.Log("Locked on Target");
+        }
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.yellow;
+        Vector3 position = transform.localPosition;
+        position.y += 0.01f;
+        Gizmos.DrawWireSphere(position, targetingRange);
+        if (target != null) {
+            Gizmos.DrawLine(position, target.Position);
+        }
+    }
+
+    bool AcquireTarget() {
+        Vector3 a = transform.localPosition;
+        Vector3 b = a;
+        b.y += 3f;
+
+        int hits = Physics.OverlapCapsuleNonAlloc(
+            a, b, targetingRange, targetsBuffer, enemyLayerMask
+        );
+        if (hits > 0) {
+            target = targetsBuffer[0].GetComponent<TargetPoint>();
+            Debug.Assert(target != null, "Targeted non-enemy!", targetsBuffer[0]);
+            return true;
+        }
+        target = null;
+        return false;
+    }
+
+    bool TrackTarget() {
+        if (target == null) {
+            return false;
+        }
+        Vector3 a = transform.localPosition;
+        Vector3 b = target.Position;
+        float x = a.x - b.x;
+        float z = a.z - b.z;
+        float r = targetingRange + 0.125f * target.Enemy.Scale;
+
+        if (x * x + z * z > r * r) {
+            target = null;
+            return false;
+        }
+        return true;
+    }
+}

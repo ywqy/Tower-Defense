f82dce9bbffac26ac8e9a3526e3cbf65c202a232
ywqy <aaawuwu@126.com>
2019-10-07 16:09:45 +0800

    Target Point

---
+++ 03 Towers/02 Targeting Enemies/Assets/Scripts/TargetPoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetPoint : MonoBehaviour {
+    public Enemy Enemy { get; private set; }
+    public Vector3 Position => transform.position;
+
+    void Awake() {
+        Enemy.transform.root.GetComponent<Enemy>();
+        Debug.Assert(Enemy != null, "Target point without Enemy root!", this);
+        Debug.Assert(
+            GetComponent<SphereCollider>() != null, 
+            "Target point without sphere collider!", this
+        );
+    }
+}

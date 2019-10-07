b7354de7a4842769a4b5489be3d0f7c4a910d08f
ywqy <aaawuwu@126.com>
2019-10-07 15:58:45 +0800

    Targeting Enemies

---
+++ 03 Towers/02 Targeting Enemies/Assets/Scripts/GameTileContent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTileContent : MonoBehaviour
+{
+    [SerializeField]
+    GameTileContentType type = default;
+
+    GameTileContentFactory originFactory;
+
+    public GameTileContentType Type => type;
+    public GameTileContentFactory OriginFactory {
+        get => originFactory;
+        set {
+            Debug.Assert(originFactory == null, "Redefined origin factory!");
+            originFactory = value;
+        }
+    }
+    public bool BlocksPath => Type == GameTileContentType.Wall || Type == GameTileContentType.Tower;
+
+
+    public void Recycle() {
+        originFactory.Reclaim(this);
+    }
+}
+

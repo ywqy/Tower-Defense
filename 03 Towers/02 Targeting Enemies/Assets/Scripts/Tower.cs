c97e8b589d73ac1c9132d93bcd5e2c18c617651f
ywqy <aaawuwu@126.com>
2019-10-07 20:01:28 +0800

    Targeting Range

--- 03 Towers/02 Targeting Enemies/Assets/Scripts/Tower.cs
+++ 03 Towers/02 Targeting Enemies/Assets/Scripts/Tower.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tower : GameTileContent
-{
+
+public class Tower : GameTileContent {
+    [SerializeField, Range(1.5f, 10.5f)]
+    float targetingRange = 1.5f;
+
     public override void GameUpdate() {
         Debug.Log("Searching for target...");
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.yellow;
+        Vector3 position = transform.localPosition;
+        position.y += 0.01f;
+        Gizmos.DrawWireSphere(position, targetingRange);
     }
 }

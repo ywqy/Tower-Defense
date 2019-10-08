56466b5e75e2656e731cf3d533a052cef6373736
ywqy <aaawuwu@126.com>
2019-10-08 13:41:31 +0800

    Calculating Trajectories

---
+++ 04 Ballistics/02 Calculating Trajectories/Assets/Scripts/GameTileContent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+public class GameTileContent : MonoBehaviour
+{
+    [SerializeField]
+    GameTileContentType type = default;
+
+    GameTileContentFactory originFactory;
+
+    public GameTileContentType Type => type;
+    public GameTileContentFactory OriginFactory {
+        get => originFactory;
+        set {
+            Debug.Assert(originFactory == null, "Redefined origin factory!");
+            originFactory = value;
+        }
+    }
+    public bool BlocksPath => Type == GameTileContentType.Wall || Type == GameTileContentType.Tower;
+
+
+    public void Recycle() {
+        originFactory.Reclaim(this);
+    }
+
+    public virtual void GameUpdate() { }
+}
+

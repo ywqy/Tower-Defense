b0e3b01c1e163574be67a9668a2474f7442d9d45
ywqy <aaawuwu@126.com>
2019-10-08 19:03:49 +0800

    Aiming Horizontally

--- 04 Ballistics/02 Calculating Trajectories/Assets/Scripts/MortarTower.cs
+++ 04 Ballistics/02 Calculating Trajectories/Assets/Scripts/MortarTower.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MortarTower : Tower
-{
+public class MortarTower : Tower {
     [SerializeField, Range(0.5f, 2f)]
     float shotsPreSecond = 1f;
 
@@ -11,4 +10,33 @@ public class MortarTower : Tower
     Transform mortar = default;
 
     public override TowerType TowerType => TowerType.Mortar;
+
+    public override void GameUpdate() {
+        Launch(new Vector3(3f, 0f, 0f));
+        Launch(new Vector3(0f, 0f, 1f));
+        Launch(new Vector3(1f, 0f, 1f));
+        Launch(new Vector3(3f, 0f, 1f));
+    }
+
+    public void Launch(Vector3 offset) {
+        Vector3 launchPoint = mortar.position;
+        Vector3 targetPoint = launchPoint + offset;
+        targetPoint.y = 0f;
+
+        Vector2 dir;
+        dir.x = targetPoint.x - launchPoint.x;
+        dir.y = targetPoint.z - launchPoint.z;
+
+        float x = dir.magnitude;
+        float y = -launchPoint.y;
+        dir /= x;
+
+        Debug.DrawLine(launchPoint, targetPoint, Color.yellow);
+        Debug.DrawLine(
+            new Vector3(launchPoint.x, 0.01f, launchPoint.z),
+            new Vector3(launchPoint.x + dir.x * x, 0.01f, launchPoint.z + dir.y * x),
+            Color.white
+        );
+
+    }
 }

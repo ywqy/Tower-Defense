dc02d791b34828e7a73de91453f137a18ac8d97b
ywqy <aaawuwu@126.com>
2019-10-08 13:39:23 +0800

    Mortar Tower

---
+++ 04 Ballistics/01 Tower Types/Assets/Scripts/MortarTower.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MortarTower : Tower
+{
+    [SerializeField, Range(0.5f, 2f)]
+    float shotsPreSecond = 1f;
+
+    [SerializeField]
+    Transform mortar = default;
+
+    public override TowerType TowerType => TowerType.Mortar;
+}
